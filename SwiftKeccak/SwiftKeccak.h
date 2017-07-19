89565c4ea5fd61da7f1bc86ddebab5c7fc6078b0
Pelle Braendgaard <pelle@stakeventures.com>
2017-07-19 12:46:22 -0400

    Initial Commit

---
+++ SwiftKeccak/SwiftKeccak.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKeccak.h
+//  SwiftKeccak
+//
+//  Created by Pelle Steffen Braendgaard on 7/19/17.
+//  Copyright © 2017 Consensys AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftKeccak.
+FOUNDATION_EXPORT double SwiftKeccakVersionNumber;
+
+//! Project version string for SwiftKeccak.
+FOUNDATION_EXPORT const unsigned char SwiftKeccakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKeccak/PublicHeader.h>
+
+
