0ab643e2568ecbf29fb38a12ddbafb7f7cda5946
Pelle Braendgaard <pelle@stakeventures.com>
2017-07-25 11:36:28 -0600

    rename sha3 in c implementation to Mecca

--- SwiftKeccak/Keccak.swift
+++ SwiftKeccak/Keccak.swift
@@ -13,7 +13,7 @@ public func keccak256(_ data: Data) -> Data {
     let nsData = data as NSData
     let input = nsData.bytes.bindMemory(to: UInt8.self, capacity: data.count)
     let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
-    sha3_256(result, 32, input, data.count)
+    keccak_256(result, 32, input, data.count)
     return Data(bytes: result, count: 32)
 }
 
