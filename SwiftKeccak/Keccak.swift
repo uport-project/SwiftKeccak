3a448c9c70edce00c887b61ca52ada0c03192f22
Pelle Braendgaard <pelle@stakeventures.com>
2017-07-25 11:56:23 -0600

    Add support for sha3 final

--- SwiftKeccak/Keccak.swift
+++ SwiftKeccak/Keccak.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import keccaktiny
+import sha3tiny
 
 public func keccak256(_ data: Data) -> Data {
     let nsData = data as NSData
@@ -21,14 +22,34 @@ public func keccak256(_ string: String) -> Data {
     return keccak256(string.data(using: String.Encoding.utf8)!)
 }
 
+public func sha3Final256(_ data: Data) -> Data {
+    let nsData = data as NSData
+    let input = nsData.bytes.bindMemory(to: UInt8.self, capacity: data.count)
+    let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
+    sha3_256(result, 32, input, data.count)
+    return Data(bytes: result, count: 32)
+}
+
+public func sha3Final256(_ string: String) -> Data {
+    return sha3Final256(string.data(using: String.Encoding.utf8)!)
+}
+
 public extension Data {
     public func keccak() -> Data {
         return keccak256(self)
     }
+
+    public func sha3Final() -> Data {
+        return sha3Final256(self)
+    }
+
 }
 
 public extension String {
     public func keccak() -> Data {
         return keccak256(self)
+    }
+    public func sha3Final() -> Data {
+        return sha3Final256(self)
     }
 }
